fd9a8c8fca91f4b148c14429124129488de6d97b
Harshika Singhal <harshikaq2609@gmail.com>
2024-01-17 20:57:00 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+with open("README.md","r",encoding="utf-8")as f:
+    long_description=f.read()
+    
+__version__="0.0.0"
+REPO_NAME="Text-summarizer_project"
+AUTHOR_USERNAME="harshika0926"
+
